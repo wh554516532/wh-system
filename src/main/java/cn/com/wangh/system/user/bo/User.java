47fd884e7558be4f58477830ab264b63cf134c3a
wh554516532 <554516532@163.com>
2017-06-24 23:14:14 +0800

    项目初始化
    项目初始化

---
+++ src/main/java/cn/com/wangh/system/user/bo/User.java
@@ -0,0 +1,68 @@
+package cn.com.wangh.system.user.bo;
+
+import java.sql.Timestamp;
+
+import cn.com.wangh.core.bo.BaseEntity;
+import cn.com.wangh.core.enums.EntityStatus;
+
+public class User extends BaseEntity {
+
+	private static final long serialVersionUID = -2846977170636930082L;
+
+	private String name;
+	
+	private String username;
+	
+	private String password;
+	
+	private Timestamp loginLastTime;
+
+	public User(String name, String username, String password, Timestamp loginLastTime) {
+		super();
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.loginLastTime = loginLastTime;
+	}
+
+	public User(String id, Timestamp created, Timestamp updated, Long version, EntityStatus status, String name,
+			String username, String password, Timestamp loginLastTime) {
+		super(id, created, updated, version, status);
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.loginLastTime = loginLastTime;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getLoginLastTime() {
+		return loginLastTime;
+	}
+
+	public void setLoginLastTime(Timestamp loginLastTime) {
+		this.loginLastTime = loginLastTime;
+	}
+}
